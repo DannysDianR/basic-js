b7e6910b76018ada24dde444c83b4bcb66829d77
DannysDianR <dannysdianrach@gmail.com>
2023-01-29 13:02:35 +0700

    Latihan js pertama kali

---
+++ algoritmapseudocode/index.js
@@ -0,0 +1,70 @@
+//ALGORITMA dan PSEUDOCODE
+// Contoh Soalnya
+// A. Tentukan hasil luas segitiga
+
+/**
+ * ALGORITMA
+ *  1. Rumus luas segitiga = 0.5 * alas * tinggi
+ *  2. Buat variable Alas
+ *  3. Buat variable tinggi
+ *  4. Buat variable luas
+ *  5. Isi variable luas dengan rumus 0.5 kali alas kali tinggi
+ *  6. Hitung luas segitiga
+ *  7. Cetak luas segitiga
+ *
+ *
+ * PSEUDOCODE
+ * ========================================
+ * // STORE bisa diganti dengan SET
+ * // PRINT bisa diganti dengan DISPLAY
+ *
+ * STORE alas with Any Number
+ * STORE tinggi with any Number
+ *
+ * SET luas with 0.5 MULTIPLY alas MULTIPLY tinggi
+ * Display luas
+ *
+ */
+
+// console.log("Hello World!");
+
+/* Contoh coding simple JS Luas Segitiga
+
+var alas = 10;
+var tinggi = 5;
+
+var luas = 0.5 * alas * tinggi;
+console.log(luas);
+*/
+
+/*Contoh coding simple JS luas Lingkaran
+var phi = 3.14;
+var r = 10;
+
+var luas = phi * r * r;
+
+console.log(luas);
+*/
+
+/*Contoh coding simple JS Luas Permukaan Tabung
+var phi = 3.14;
+var r = 7;
+var t = 5;
+
+var luas = 2 * phi * r * (r + t);
+
+console.log(luas);*/
+
+/*Contoh komppleks Luas Permukaan Tabung (Source Google)
+function luas_Permukaan_Tabung(phi, r, t) {
+  let L = 2 * phi * r * (r + t);
+  return L;
+}
+
+let phi = 3.14;
+let r = 10;
+let t = 10;
+
+let luasPermukaanTabung = luas_Permukaan_Tabung(phi, t, r);
+
+console.log(luasPermukaanTabung);*/
