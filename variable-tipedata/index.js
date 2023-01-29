4aacbca7864e8b4c78031f5bbc55a53fe188f05b
DannysDianR <dannysdianrach@gmail.com>
2023-01-29 15:05:29 +0700

    pembelajaran variable

---
+++ variable-tipedata/index.js
@@ -0,0 +1,29 @@
+// Variable dan Tipe Data
+
+/**
+ * TIPE DATA
+ *
+ * Number
+ * String
+ * Boolean
+ * Object
+ * Undefined
+ * Null
+ *
+ */
+
+// Operator ada 2: Operator Math dan Operator Logic
+
+// Operator Math
+// +, -, *, /, % (modulus = sisa bagi)
+
+// Operator Logic
+// ==,!=, >, <, >=, <=
+
+/*Contoh Operator Logic
+var a = 5;
+var b = "5";
+
+=== ini untuk membandingkan tidak hanya nilai, namun juga tipe data
+var result = a === b;
+console.log(result);*/
