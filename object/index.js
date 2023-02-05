2515fb62df392b4667a459e87e40cc5ec14e9769
DannysDianR <dannysdianrach@gmail.com>
2023-02-05 15:54:07 +0700

    materi dari object

--- object/index.js
+++ object/index.js
@@ -7,20 +7,91 @@ let book = ["JS for dummies", "learning", 150000,5,true]
 
 */
 
-function bookObject(arr) {
+// function bookObject(arr) {
+//   let result = {};
+
+//   result.name = arr[0];
+//   result.type = arr[1];
+//   result.price = arr[2];
+//   result.stock = arr[3];
+//   result.isAvailable = arr[4];
+
+//   //Kalo tidak ada return variable, maka nanti dioutput tidak akan menghasilkan apa" (Cuman object dan function)
+
+//   // tidak ada return result = undefined.
+//   return result;
+// }
+
+// //Jangan lupa [] karena bentuknya array
+// console.log(bookObject(["JS for Dummies", "Learning", 150000, 5, true]));
+
+// function bookObject(arr) {
+//   let result = {};
+
+//   result.name = arr[0];
+//   result.type = arr[1];
+//   result.price = arr[2];
+//   result.stock = arr[3];
+//   result.isAvailable = arr[4];
+
+//   return result;
+// }
+
+// let file = ["JS For Expert", "Expensive", 2000000, 10, true];
+
+// console.log(bookObject(file));
+
+const countVote = (votes) => {
+  // code here
   let result = {};
 
-  result.name = arr[0];
-  result.type = arr[1];
-  result.price = arr[2];
-  result.stock = arr[3];
-  result.isAvailable = arr[4];
+  if (votes.length === 0) {
+    return "tidak ada suara.";
+  }
 
-  //Kalo tidak ada return variable, maka nanti dioutput tidak akan menghasilkan apa" (Cuman object dan function)
+  for (let i = 0; i < votes.length; i += 1) {
+    //Kondisi jika si votes tidak ada selain 1 suara / ada nambah orang baru tapi cuman 1.
+    if (result[votes[i]] === undefined) {
+      result[votes[i]] = 1;
+    } else {
+      result[votes[i]] += 1;
+    }
+  }
 
-  // tidak ada return result = undefined.
-  return result;
-}
+  let winner = 0;
+  let winnerKey = "";
+  /*
+    Kondisi for nya yaitu bakal Loop sesuai dengan result / jumlah orang yg dimasukin di votes.
+  
+  */
+  for (let key in result) {
+    /*Kondisinya
+      Jika winner < vincent[3]
+      Jika 0 < vincent[3], maka jalankan kondisinya
+    */
+    if (winner < result[key]) {
+      // winnernya berubah menjadi nama/result dari key yg terbanyak.
+      winner = result[key];
 
-//Jangan lupa [] karena bentuknya array
-console.log(bookObject(["JS for Dummies", "Learning", 150000, 5, true]));
+      // winnerKey nya juga terubah berdasarkan banyaknya key tersebut. (Konteksnya disini angka terbesar)
+      winnerKey = key;
+    }
+  }
+
+  return `Pemenang adalah ${winnerKey} dengan ${winner} suara.`;
+};
+
+//Test Case
+console.log(
+  countVote([
+    "vincent",
+    "ncent",
+    "vincent",
+    "james",
+    "ncent",
+    "james",
+    "vincent",
+  ])
+);
+
+console.log(countVote([]));
